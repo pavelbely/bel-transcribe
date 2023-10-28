37140e059f373c418e5c7859f9dd456d5b58798a
Pavel Bely <paviel.biely@gmail.com>
2023-10-28 19:24:21 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+
+import re
+import json
+from pathlib import Path
+
+def get_replacer(pattern, replacement):
+  def replacer(match):
+    g = match.group()
+    replaced_str= re.sub(pattern, replacement, g, flags=re.IGNORECASE)
+    if replaced_str.islower(): return replaced_str.lower()
+    if replaced_str.istitle(): return replaced_str.title()
+    if replaced_str.isupper(): return replaced_str.upper()
+    return replaced_str
+  return replacer
+
+def replace_text_with_replacements(input_text, replacements, is_full_word = False):
+  result = input_text
+  for key in replacements.keys():
+    pattern = (r'\b(' if is_full_word else '(') + key + (r')\b' if is_full_word else ')')
+    result = re.sub(pattern, get_replacer(pattern, replacements.get(key)), result, flags=re.IGNORECASE)
+  return result
+
+#######################################################################
+input_file = 'input.srt'
+output_file = 'output.srt'
+#######################################################################
+
+maps_json = Path(__file__).with_name('maps.json')
+with open('maps.json') as json_data:
+    maps = json.load(json_data)
+    json_data.close()
+
+infile_path = Path(__file__).with_name(input_file)
+outfile_path = Path(__file__).with_name(output_file)
+with open(infile_path, 'r', encoding='utf-8') as infile, open(outfile_path, 'w', encoding='utf-8') as outfile:
+    lines = infile.readlines()
+    for line in lines:
+        line = replace_text_with_replacements(line, maps['word_map'], True)
+        line = replace_text_with_replacements(line, maps['letter_map'])
+        line = replace_text_with_replacements(line, maps['syllable_map'])
+        line = replace_text_with_replacements(line, maps['syllable_group_map'])
+        outfile.write(line)
+
+print(f"Word replacements complete. Modified file saved to {output_file}")
