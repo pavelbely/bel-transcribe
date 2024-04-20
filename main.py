66c6ff4f7ee0e299c51cd0137a61ecd2bab3dce7
Pavel Bely <paviel.biely@gmail.com>
2024-04-20 14:36:27 +0300

    Extract code into a function

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import re
 import json
 from pathlib import Path
 
+maps_json = Path(__file__).with_name('maps.json')
+with open('maps.json') as json_data:
+    maps = json.load(json_data)
+    json_data.close()
+
 def get_replacer(pattern, replacement):
   def replacer(match):
     g = match.group()
@@ -24,25 +29,17 @@ def replace_text_with_replacements(input_text, replacements, is_full_word = Fals
     result = re.sub(pattern, get_replacer(pattern, replacements.get(key)), result, flags=re.IGNORECASE)
   return result
 
-#######################################################################
-input_file = 'input.srt'
-output_file = 'output.srt'
-#######################################################################
+def replace(input_file, output_file):
+  infile_path = Path(__file__).with_name(input_file)
+  outfile_path = Path(__file__).with_name(output_file)
+  with open(infile_path, 'r', encoding='utf-8') as infile, open(outfile_path, 'w', encoding='utf-8') as outfile:
+      lines = infile.readlines()
+      for line in lines:
+          line = replace_text_with_replacements(line, maps['letter_map'])
+          line = replace_text_with_replacements(line, maps['syllable_map'])
+          line = replace_text_with_replacements(line, maps['syllable_group_map'])
+          line = replace_text_with_replacements(line, maps['word_map'], True)
+          outfile.write(line)
+  print(f"Word replacements complete. Modified file saved to {output_file}")
 
-maps_json = Path(__file__).with_name('maps.json')
-with open('maps.json') as json_data:
-    maps = json.load(json_data)
-    json_data.close()
-
-infile_path = Path(__file__).with_name(input_file)
-outfile_path = Path(__file__).with_name(output_file)
-with open(infile_path, 'r', encoding='utf-8') as infile, open(outfile_path, 'w', encoding='utf-8') as outfile:
-    lines = infile.readlines()
-    for line in lines:
-        line = replace_text_with_replacements(line, maps['letter_map'])
-        line = replace_text_with_replacements(line, maps['syllable_map'])
-        line = replace_text_with_replacements(line, maps['syllable_group_map'])
-        line = replace_text_with_replacements(line, maps['word_map'], True)
-        outfile.write(line)
-
-print(f"Word replacements complete. Modified file saved to {output_file}")
+replace('input.srt', 'output.srt')
